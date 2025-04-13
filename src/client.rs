987bc418416af94e27ea9c3fa6bfcf4eeabd28e3
FintasticMan <finlay.neon.kid@gmail.com>
2025-04-13 12:26:02 +0200

    I don't like utils files

--- src/client.rs
+++ src/client.rs
@@ -7,7 +7,16 @@ use serde_json::Value as JsonValue;
 use url::Url;
 
 use crate::record::{self, Content, Record, Type};
-use crate::{split_domain, ApiError, ClientBuilderError, ClientError, DomainError, PayloadBuilder};
+use crate::{ApiError, ClientBuilderError, ClientError, DomainError, PayloadBuilder};
+
+fn split_domain<'a>(name: &'a domain::Name) -> Result<(Option<&'a str>, &'a str), DomainError> {
+    let root = name
+        .root()
+        .ok_or_else(|| DomainError::MissingRoot(name.to_string()))?;
+    let prefix = name.prefix();
+
+    Ok((prefix, root))
+}
 
 pub struct ClientBuilder {
     endpoint: Option<Url>,
--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,7 @@ mod client;
 mod errors;
 mod payload;
 pub mod record;
-mod utils;
 
 pub use client::*;
 pub use errors::*;
 pub(crate) use payload::*;
-pub(crate) use utils::*;
