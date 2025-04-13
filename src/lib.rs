cf5665bbf5138b9e145a63ea3efb935a04faeda1
FintasticMan <finlay.neon.kid@gmail.com>
2025-04-13 22:20:39 +0200

    Add some documentation, small refactorings

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,31 @@
+//! # Simple and type-safe client for the Porkbun API.
+//!
+//! Implements an easy-to-use client for interfacing with the [Porkbun API].
+//! Ensures that correct values are supplied using the Rust type system.
+//!
+//! ## Examples
+//!
+//! See [hamsando-ddns] for an implementation of a dynamic DNS program using this crate.
+//!
+//! ```
+//! use addr::parse_domain_name;
+//! use hamsando::Client;
+//!
+//! let client = Client::builder()
+//!     .apikey("<APIKEY>")
+//!     .secretapikey("<SECRETAPIKEY>")
+//!     .build()
+//!     .unwrap();
+//!
+//! let my_ip = client.test_auth().unwrap();
+//!
+//! let domain = parse_domain_name("example.com").unwrap();
+//! let record_id = client.create_dns(&domain, &my_ip.into(), None, None).unwrap();
+//! ```
+//!
+//! [Porkbun API]: https://porkbun.com/api/json/v3/documentation
+//! [hamsando-ddns]: https://github.com/FintasticMan/hamsando-ddns
+
 mod client;
 mod errors;
 mod payload;
