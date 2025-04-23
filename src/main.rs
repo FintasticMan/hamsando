FintasticMan/porkbun-rs

Finish API impl, split up a bit

src/main.rs
@@ -1,4 +1,4 @@
-use porkbun::{Client, Content};
+use porkbun::{record::Content, Client};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config = config::Config::builder()
@@ -11,10 +11,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let client = Client::new(config);
 
-    println!(
-        "{}",
-        client.create_dns("riverhill.xyz", Some(""), Content::A("0.0.0.0".parse()?))?
-    );
+    dbg!(client.retrieve_dns("riverhill.xyz", None)?);
 
     Ok(())
 }

FintasticMan/hamsando

src/main.rs
@@ -1,4 +1,4 @@
-use porkbun::{Client, Content};
+use porkbun::{record::Content, Client};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config = config::Config::builder()
@@ -11,10 +11,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let client = Client::new(config);
 
-    println!(
-        "{}",
-        client.create_dns("example.com", Some(""), Content::A("0.0.0.0".parse()?))?
-    );
+    dbg!(client.retrieve_dns("example.com", None)?);
 
     Ok(())
 }